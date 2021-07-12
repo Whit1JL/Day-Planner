91e8635bb0bbf628dc65e47f01e99c371422cd45
Jessica White <jessica2714white@gmail.com>
2021-07-11 19:27:55 -0500

    Omitted some items

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
 // jquery 
 
+// user when loading page, current date appear at top of screen
+// user scroll down, see time blocks 
+
+
+when the user loads the page the current date should appear on top of calender 
+when the user scrolls down, they are presented with time blocks for standard business hours 9-5
+the time blocks are color coded as follows:
+    past- grey
+    present- red
+    future- green
+user can click into the time blocks and type in data
+    user then clicks the save button 
+    data input for that event is saved into local storage drive
+    when the page is refreshed the calender is updated with recent information changes
+*/
+
+// use moment js for math calculation for determining time
+
+
+
 $(document).ready(function () {
     // all of the code is going inside brackets
     console.log("document ready")
