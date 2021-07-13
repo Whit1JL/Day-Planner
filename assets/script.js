f0ad2f4d867b0e93af2903d651a80bae7c0205a4
Jessica White <jessica2714white@gmail.com>
2021-07-12 22:05:40 -0500

    Created local storage

--- assets/script.js
+++ assets/script.js
@@ -23,42 +23,4 @@ $(document).ready(function () {
 
     });
 
-    // loops through the id array of times and grabs data from local storage 
-    let timeClock = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
-    // times in military style
-    let milTime = [9,10,11,12,13,14,15,16,17]; 
-    // created a for loop to save items into local storage
-    // saved event so even when page is refreshed it still works
-    for (let i = 0; i < timeClock.length; i++) {
-        savedEvent = $(".saved-event");
-        if (localStorage.getItem(timeClock[i])) {
-            $(timeClock[i]).val(localStorage.getItem(timeClock[i]))
-        };
-    };
-
-    //changing style block color based on time of day
-    function styleHour() {
-        var currentHour = moment().hour();
-        //loop over time blocks
-        for (let i = 0; i < timeClock.length; i++) {
-            
-            //check if time is present (red)
-            if (milTime[i] === currentHour) {
-                $(timeClock[i]).addClass("present");
-
-            } 
-            //check if time is past (gray)
-            else if(milTime[i] >= currentHour) {
-                $(timeClock[i]).addClass("future");
-            }
-            //check if time is future (green)
-            else {
-                $(timeClock[i]).addClass("past");
-            };
-            
-        }     
-        
-    };
-    styleHour();
-
-});
+   
